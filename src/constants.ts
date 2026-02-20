f43653292fbf97e67fc57382a110e295f366af19
Dusterito <57031701+AshleyFitness@users.noreply.github.com>
2026-02-20 11:57:37 +0100

    fixing typo

--- src/constants.ts
+++ src/constants.ts
@@ -7,4 +7,4 @@ export const POPUP_WIDTH = 500;
 export const POPUP_HEIGHT = 700;
 export const POPUP_TIMEOUT = 15 * 60 * 1000; // 15 minutes
 export const MESSAGE_SOURCE = "tip4serv-checkout";
-export const LOGO_URL = "https://js.tip4serv.com/t4s_logo_gray.webp";
+export const LOGO_URL = "https://js.tip4serv.com/assets/t4s_logo_gray.webp";
